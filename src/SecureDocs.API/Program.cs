21bd762276bf3b3a19fdf9978958df2eab756e58
martumucci <martina22.12@hotmail.com>
2026-05-11 12:57:47 -0300

    mediatr-and-fluentvalidation

--- src/SecureDocs.API/Program.cs
+++ src/SecureDocs.API/Program.cs
@@ -1,11 +1,20 @@
+using FluentValidation;
+using MediatR;
+using SecureDocs.Application.Common.Behaviors;
+using SecureDocs.Application.Documents.Commands.SubmitDocument;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMediatR(cfg =>
+    cfg.RegisterServicesFromAssemblyContaining<SubmitDocumentCommand>());
+
+builder.Services.AddValidatorsFromAssemblyContaining<SubmitDocumentCommand>();
+
+builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
 
 var app = builder.Build();
 
